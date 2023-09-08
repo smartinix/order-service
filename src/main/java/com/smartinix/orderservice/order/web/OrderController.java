aeb30c0986ea7774c2a0e223705d50bcd42a7a9a
Nenad Martinovic <nenad.martinovic@enjoying.rs>
2023-09-08 18:30:40 +0200

    Added order-service initial impl

---
+++ src/main/java/com/smartinix/orderservice/order/web/OrderController.java
@@ -0,0 +1,36 @@
+package com.smartinix.orderservice.order.web;
+
+import com.smartinix.orderservice.order.domain.Order;
+import com.smartinix.orderservice.order.domain.OrderService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("orders")
+public class OrderController {
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping
+    public Flux<Order> getAllOrders() {
+        return orderService.getAllOrders();
+    }
+
+    @PostMapping
+    public Mono<Order> submitOrder(
+        @RequestBody @Valid OrderRequest orderRequest
+    ) {
+        return orderService.submitOrder(
+            orderRequest.isbn(), orderRequest.quantity()
+        );
+    }
+}
