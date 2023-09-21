dd29e4abaf3aef0f21aff2b7f383832d4e51c761
Nenad Martinovic <nenad.martinovic@enjoying.rs>
2023-09-21 12:37:10 +0200

    Additional configuration for logging, actuator, etc...

--- src/main/java/com/smartinix/orderservice/config/SecurityConfig.java
+++ src/main/java/com/smartinix/orderservice/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.smartinix.orderservice.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -8,12 +9,14 @@ import org.springframework.security.web.server.savedrequest.NoOpServerRequestCac
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+@Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
     @Bean
     SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
         return http
             .authorizeExchange(exchange -> exchange
+                .pathMatchers("/actuator/**").permitAll()
                 .anyExchange().authenticated()
             )
             .oauth2ResourceServer(
